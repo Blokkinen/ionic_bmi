4e98e2b15174bcf95ee0e1161d89ae07fe32b336
blokkinen <viktoria_blok@hotmail.com>
2016-09-19 15:43:11 +0200

    created tabs for the views

--- bmi_calculator/www/js/app.js
+++ bmi_calculator/www/js/app.js
@@ -31,28 +31,42 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   // Each state's controller can be found in controllers.js
   $stateProvider
 
+  .state('tab.bmi', {
+    url: '/bmi',
+    views: {
+      'tab-bmi': {
+        templateUrl: 'templates/calculator/calculator.html',
+        controller: 'BmiController'
+      }
+    }
+  })
+
+  .state('tab.about', {
+    url: '/about',
+    views: {
+      'tab-about': {
+        templateUrl: 'templates/about/about.html',
+        controller: 'AboutController'
+      }
+    }
+  })
+
+
   // setup an abstract state for the tabs directive
     .state('tab', {
     url: '/tab',
     abstract: true,
     templateUrl: 'templates/tabs.html'
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
   });
 
 
 
+  // Each tab has its own nav history stack:
+
+
+
+
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/about');
 
 });
