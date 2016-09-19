6609079d55e036189b0f9e53072e09b82fe34e0f
blokkinen <viktoria_blok@hotmail.com>
2016-09-19 16:14:52 +0200

    Added the functionality

--- bmi_calculator/www/js/controllers.js
+++ bmi_calculator/www/js/controllers.js
@@ -7,3 +7,15 @@ var bmi = angular.module('starter.controllers', []);
   bmi.controller('BmiController', function() {
 
   });
+
+  bmi.controller('BmiController', function($scope) {
+  $scope.data = {};
+  $scope.calculateBMI = function() {
+    var person = new Person({
+      weight: $scope.data.weight,
+      height: $scope.data.height
+    });
+    person.calculate_bmi_met();
+    $scope.person = person;
+  };
+});
